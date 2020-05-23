bd70db13b773014d9b4974154cc40f2b78e5ff8b
Emannuel Carvalho <emannuel.carvalho@icloud.com>
2020-05-23 20:13:48 -0300

    Add dynamic informations screen

---
+++ Nina/DynamicViewController.swift
@@ -0,0 +1,94 @@
+//
+//  DynamicViewController.swift
+//  Nina
+//
+//  Created by Emannuel Carvalho on 21/05/20.
+//  Copyright © 2020 Emannuel Carvalho. All rights reserved.
+//
+
+import UIKit
+import Bifrost
+import os.log
+
+class DynamicViewController: UIViewController {
+    
+    let urlString: String
+    let log = Nina.log(for: DynamicViewController.self)
+    
+    init(urlString: String = "http://localhost:8080/screen") {
+        self.urlString = urlString
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(fetchView)))
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        fetchView()
+    }
+    
+    @objc fileprivate func fetchView() {
+        let session = URLSession.shared
+        guard let url = URL(string: urlString) else { return }
+        let task = session.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                if let error = error {
+                    os_log(.error, log: self.log, "Failed to retrieve dynamic screen: %{PUBLIC}@", "\(error)")
+                }
+                return
+            }
+            let jsonResult = try? JSONSerialization.jsonObject(with: data, options:.mutableLeaves)
+            if let jResult = jsonResult as? [String: Any] {
+                DispatchQueue.main.async {
+                    do {
+                        let comp = try DynamicComponent.parse(dictionary: jResult) as DynamicComponent
+                        let view = try DynamicView.createView(dynamicsComponent: comp, actionDelegate: self)
+                        for subview in self.view.subviews {
+                            subview.removeFromSuperview()
+                        }
+                        view.fill(view: self.view)
+                    } catch {
+                        os_log(.error, log: self.log, "Failed to create dynamic screen: %{PUBLIC}@", "\(error)")
+                    }
+                }
+            }
+        }
+        task.resume()
+        
+    }
+    
+}
+
+extension DynamicViewController: DynamicActionDelegate {
+    func callAction(sender: String) {
+        handle(action: sender)
+    }
+}
+
+extension DynamicActionDelegate where Self: UIViewController {
+    
+    func handle(action: String) {
+        if action == "dismiss" {
+            dismiss(animated: true, completion: nil)
+        } else if action.hasPrefix("showScreen") {
+            if let url = action.components(separatedBy: "Screen:").last {
+                let vc = DynamicViewController(urlString: url)
+                navigationController?.show(vc, sender: nil)
+            }
+        } else if action.hasPrefix("presentScreen") {
+            if let url = action.components(separatedBy: "Screen:").last {
+                let vc = DynamicViewController(urlString: url)
+                present(vc, animated: true, completion: nil)
+            }
+        }
+    }
+    
+}
--- Nina/SceneDelegate.swift
+++ Nina/SceneDelegate.swift
@@ -12,17 +12,33 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let todayViewController = InitialViewController()
         let resultsViewController = ResultsViewController()
         let nav = RegularNavigationController()
         nav.viewControllers = [resultsViewController, todayViewController]
+        nav.title = "Progresso"
+        nav.tabBarItem = UITabBarItem(title: "Progresso",
+                                      image: UIImage(named: "NinaTab2"),
+                                      selectedImage: UIImage(named: "NinaTab"))
+        
+        let dynamic = DynamicViewController(urlString: Secrets.screenURL)
+        dynamic.title = "Informações"
+        dynamic.tabBarItem = UITabBarItem(title: "Informações",
+                                          image: UIImage(systemName: "bell"),
+                                          selectedImage: UIImage(systemName: "bell.fill"))
+        let nav2 = RegularNavigationController()
+        nav2.viewControllers = [dynamic]
+        
+        let tab = UITabBarController()
+        tab.view.tintColor = Color.primaryButton
+        tab.viewControllers = [nav, nav2]
         
         
-        window.rootViewController = nav
+        window.rootViewController = tab
         self.window = window
         window.makeKeyAndVisible()
     }
