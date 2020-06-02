0dfa438109653ff18e2f48130e78a386aec61d2e
Emannuel Carvalho <emannuel.carvalho@icloud.com>
2020-06-02 18:43:25 -0300

    Fix exercise summary view

--- Nina/InitialFlow/ExerciseSummaryViewController.swift
+++ Nina/InitialFlow/ExerciseSummaryViewController.swift
@@ -13,6 +13,14 @@ class ExerciseSummaryViewController: UIViewController {
     let minutes: Double
     let date: Date
     
+    lazy var titleView: SimpleTitleView = SimpleTitleView(title: "Exercícios").notTranslating()
+    
+    lazy var textLabel: UILabel = {
+        let label = UILabel().notTranslating()
+        label.numberOfLines = 0
+        return label
+    }()
+    
     init(minutes: Double, date: Date) {
         self.minutes = minutes
         self.date = date
@@ -26,6 +34,19 @@ class ExerciseSummaryViewController: UIViewController {
     
     fileprivate func setupViews() {
         view.backgroundColor = Color.secondary
+        
+        view.addSubview(titleView)
+        view.addSubview(textLabel)
+        
+        titleView.attachToTop(at: view, margin: Margin.default)
+        titleView.heightAnchor.constraint(equalToConstant: 56).isActive = true
+        titleView.titleLabel.textAlignment = .center
+        
+        textLabel.topAnchor.constraint(equalTo: titleView.bottomAnchor, constant: 20).isActive = true
+        textLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Margin.default).isActive = true
+        textLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Margin.default).isActive = true
+        
+        
         var suffix = "hoje."
         if !Calendar.current.isDateInToday(date) {
             let formatter = DateFormatter()
@@ -35,10 +56,9 @@ class ExerciseSummaryViewController: UIViewController {
         let text = "Como você usa Apple Watch, é possível obter o seu tempo de exercícios pelo HealthKit."
             + " Você realizou \(minutes) minutos de exercício "
             + suffix
-        view.fill(with: [
-            SimpleTitleView(title: "Exercícios"),
-            SimpleTextView(text: text)
-        ])
+        
+        textLabel.text = text
+        
     }
     
 }
